e41859f6484d50703f6c36cee5d358f98485d566
Mansur <mansurv@mail.ru>
2020-11-23 19:19:13 +0300

    Next update

--- LookatNet_22_11_20_w_bottomnav_donotwork/app/src/main/java/com/netmontools/lookatnet/ui/local/view/RecyclerViewClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.netmontools.lookatnet.ui.local.view;
-
-import android.view.View;
-
-public interface RecyclerViewClickListener {
-    void onClick(View view, int position);
-
-    void onLongClick(View view, int position);
-}
